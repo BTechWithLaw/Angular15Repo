452ef447425292d3af7a0451c985f8abc6afe8a0
BTechWithLaw <BTechWithLaw@gmail.com>
2023-01-30 01:02:58 +0530

    Latest Changes

--- Angular15Project/src/app/app.module.ts
+++ Angular15Project/src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DatepickerComponent } from './datepicker/datepicker.component';
+import { CalenderComponent } from './calender/calender.component';
+import { TimerComponent } from './timer/timer.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatepickerComponent,
+    CalenderComponent,
+    TimerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports:[DatepickerComponent]
 })
 export class AppModule { }
