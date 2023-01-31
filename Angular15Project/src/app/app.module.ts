39ad46c94637df376b9ed4d718f711f16d623c3c
BTechWithLaw <BTechWithLaw@gmail.com>
2023-01-31 13:49:09 +0530

    http get method added

--- Angular15Project/src/app/app.module.ts
+++ Angular15Project/src/app/app.module.ts
@@ -4,27 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DatepickerComponent } from './datepicker/datepicker.component';
-import { CalenderComponent } from './calender/calender.component';
 import { TimerComponent } from './timer/timer.component';
-
+import { HttpClientModule } from '@angular/common/http';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    DatepickerComponent,
-    CalenderComponent,
     TimerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent],
-  exports:[DatepickerComponent]
 })
 export class AppModule { }
--- Angular15Project/src/app/timer/timer.component.ts
+++ Angular15Project/src/app/timer/timer.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-timer',
   templateUrl: './timer.component.html',
   styleUrls: ['./timer.component.css']
 })
-export class TimerComponent {
+export class TimerComponent implements OnInit {
 
+  data:any;
+  constructor(private http:HttpClient){
+    console.log("Constructor is initialized");
+  }
+  ngOnInit(): void {
+    debugger;
+    console.log("OnInit Method is executed");
+    this.GetData();
+  }
+
+  GetData(){
+     this.http.get("http://localhost:6210/api/Employee")
+    .subscribe(s=> this.data=s);
+    console.log(this.data);
+  }
+
+  
 }
